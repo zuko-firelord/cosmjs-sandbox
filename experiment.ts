e8fcc0ba39c96d55bb8e1b0011f136155ae0cd12
sachin tedwa <tedwasachin123@gmail.com>
2023-07-06 13:51:26 +0530

    rpc_client

--- experiment.ts
+++ experiment.ts
@@ -1,5 +1,10 @@
+import {  StargateClient } from "@cosmjs/stargate"
+
+const rpc = "https://rpc.sentry-01.theta-testnet.polypore.xyz"
+
 const runAll = async (): Promise<void> => {
-    console.log("TODO")
+    const client = await StargateClient.connect(rpc)
+    console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
 }
 
 runAll()
